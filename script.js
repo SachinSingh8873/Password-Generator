f55462f1f09c1ce4d5783a9bdd38c34161af21be
Sachin <sachinkr887368@gmail.com>
2024-11-03 22:25:18 +0530

    here i have changes some functions.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let lowercase = document.getElementById("lowercase");
 let UpperCase = document.getElementById("UpperCase");
 let symbols = document.getElementById("symbols");
 let genBtn = document.getElementById("genBtn");
-
+let copy = document.getElementById("copy");
 
 //Showing input slideer value
 sliderValue.textContent = inputSlider.value;
@@ -18,13 +18,42 @@ genBtn.addEventListener('click',()=>{
     passBox.value = generatepassword();
 })
 
+let lowerchars = "abcdefghijklmnopqrstuvwxyz";
 let upperchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allnumbers = "0123456789";
+let allsymbols = "~!@#$%^&*";
 
 //function to generate password
 
 function generatepassword(){
     let genPassword = "";
+    let allChars = "";
 
-    genPassword = Math.floor(Math.random() * upperchars.length);
+    allChars += lowercase.checked ? lowerchars : "";
+    allChars += UpperCase.checked ? upperchars: "";
+    allChars += symbols.checked ? allsymbols : "";
+    allChars += Numbers.checked ? allnumbers : "";
+
+    if (allChars== "" || allChars.length == 0){
+        return genPassword;
+    }
+
+    let i = 1;
+    while(i<=inputSlider.value){
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+        i++; 
+    }
     return genPassword;
 }
+copy.addEventListener('click', ()=>{
+    if(passBox.value != "" || passBox.value.length>=1){
+        navigator.clipboard.writeText(passBox.value);
+        copy.innerText ="check";
+        copy.title = "Password Copied";
+
+        setTimeout(()=>{
+            copy.innerHTML = "content_copy"
+            copy.title = "";
+        }, 3000)
+    }
+})
